6c5a05c222e3212bb20224ab5abbe33e2454a9b9
Oscar Saharoy <osaharoy@gmail.com>
2023-06-05 08:56:48 +0100

    starting tile

--- js/subtiles.js
+++ js/subtiles.js
@@ -1,3 +1,14 @@
-// Oscar Saharot 2023
+// Oscar Saharoy 2023
 
+class Tile {
+	constructor() {
+		this.verts = [ [0.25,0.25], [0.25,-0.25], [-0.25,-0.25], [-0.25,0.25] ];
+	}
 
+	toSVG() {
+
+		return `<path d="
+			${ vertsToD(this.verts) }
+		" />`;
+	}
+}
