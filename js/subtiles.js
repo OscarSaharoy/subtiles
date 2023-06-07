0347b3952c104e1a51f2c41cc22973526a04b80e
Oscar Saharoy <osaharoy@gmail.com>
2023-06-07 15:52:38 +0100

    enable svg upload

--- js/subtiles.js
+++ js/subtiles.js
@@ -4,10 +4,36 @@ import { TileList } from "./TileList.js";
 import { DiamondTile } from "./Tile.js";
 
 
+let svg = document.querySelector( "svg" );
+const uploadInput = document.getElementById( "upload-input" );
+
+const reader = new FileReader();
+
+window.addEventListener( "keydown",
+	e => e.key == "o" && uploadInput.click() );
+uploadInput.addEventListener( "change",
+	e => reader.readAsText( uploadInput.files[0] ) );
+reader.addEventListener( "load", 
+	e => constructRules( reader.result ) );
+
+
+function constructRules( svgText ) {
+
+	svg.outerHTML = svgText.match( /<\s*svg.*?>(.*)<\/\s*svg.*?>/is )[0];
+	
+	svg = document.querySelector( "svg" );
+	const outerGroup = svg.querySelector( "g" );
+	const innerGroup = outerGroup.querySelector( "g" );
+
+	const outerTile = outerGroup.querySelector( "path" );
+	const innerTiles = [ ...innerGroup.querySelectorAll( "path" ) ];
+
+	console.log( outerTile, innerTiles );
+}
+
+
 const rootTile = new DiamondTile();
 const tileList = new TileList( rootTile );
-
-const svg = document.querySelector( "svg" );
 
 let i = 0;
 while( i++ < 4 )
