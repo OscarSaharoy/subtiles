bcc8bfa04e6ab06d08bac8f5af7124182e429d4e
Oscar Saharoy <osaharoy@gmail.com>
2023-07-04 10:14:33 +0100

    menus looking ok

--- js/click-in.js
+++ js/click-in.js
@@ -1,10 +1,19 @@
 // Oscar Saharoy 2023
 
+
+// a space or enter keypress on a `.click-in` elm should be interpreted as a click
 const clickIns = [ ...document.querySelectorAll( "form .click-in" ) ];
 
-clickIns.forEach( 
-	elm => elm.addEventListener( "keydown", e => 
+clickIns.forEach( elm => 
+	elm.addEventListener( "keydown", e => 
 		( e.key == " " || e.key == "Enter" ) && elm.click() 
 	) 
 );
 
+
+// take focus away from other buttons when mouse enters either of the menus
+const menus = [ "preset", "mapping" ].map( s => document.getElementById( s ) );
+
+menus.forEach( elm =>
+	elm.addEventListener( "pointermove", e => document.activeElement.blur() )
+);
