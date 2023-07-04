43365a52eee9259bc3ad4ac24b462a88c5abb5e2
Oscar Saharoy <osaharoy@gmail.com>
2023-07-04 10:20:34 +0100

    preset menu working

--- js/presets.js
+++ js/presets.js
@@ -3,11 +3,19 @@
 import { constructRules } from "./upload.js";
 
 
+const presetButton = document.getElementById( "preset" );
+const presetOptions = [ ...presetButton.querySelectorAll( "p" ) ];
+
+presetOptions.forEach( elm => 
+	elm.onclick = () => fetchPreset( elm.textContent.trim() ) 
+);
+
+
 async function fetchPreset( filename ) {
 
-	const response = await fetch( "/templates/subtiles1.svg" );
+	const response = await fetch( `/presets/${filename}.svg` );
 	const svgText = await response.text();
 	constructRules( svgText );
 }
-fetchPreset();
+fetchPreset( "square" );
 
