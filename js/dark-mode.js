a51b8f192e7da2fe8558edb4e0529e56343d25d7
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2023-09-01 22:32:36 +0100

    can now toggle dark mode

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,5 +1,16 @@
 // Oscar Saharoy 2023
 
+import { recolour } from "./subdivide.js";
+
 export let darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 if( darkMode ) document.body.classList.add( "dark-mode" );
+
+function toggleDarkMode() {
+	darkMode = !darkMode;
+	document.body.classList.toggle( "dark-mode" );
+	recolour();
+}
+
+const darkModeButton = document.querySelector( "button#dark-mode" );
+darkModeButton?.addEventListener( "click", toggleDarkMode );
 
