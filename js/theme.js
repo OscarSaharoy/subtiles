53191cc856545d21bc90d7fb29f6edf1e5f5e8d0
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2023-09-01 22:11:43 +0100

    dark mode with body class

--- js/theme.js
+++ js/theme.js
@@ -1,5 +1,5 @@
 // Oscar Saharoy 2023
 
 export let darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
+if( darkMode ) document.body.classList.add( "dark" );
 
