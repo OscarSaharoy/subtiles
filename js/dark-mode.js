97267f58b8e5827f3fadd6240265b0bf8fab0f70
Oscar Saharoy <oscar.saharoy@lowcarboncontracts.uk>
2023-09-02 09:12:09 +0100

    add light mode dark mode icons

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -3,14 +3,17 @@
 import { recolour } from "./subdivide.js";
 
 export let darkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-if( darkMode ) document.body.classList.add( "dark-mode" );
-
-function toggleDarkMode() {
-	darkMode = !darkMode;
-	document.body.classList.toggle( "dark-mode" );
-	recolour();
-}
-
+const darkModeIcon = "../assets/dark-mode.svg";
+const lightModeIcon = "../assets/light-mode.svg";
 const darkModeButton = document.querySelector( "button#dark-mode" );
-darkModeButton?.addEventListener( "click", toggleDarkMode );
+darkModeButton?.addEventListener( "click", () => setDarkMode( !darkMode, true ) );
+
+function setDarkMode(newDarkMode, redraw=false) {
+	darkMode = newDarkMode;
+	document.body.classList.toggle( "dark-mode" );
+	if(redraw) recolour()
+	darkModeButton.querySelector( "img" ).src =
+		darkMode ? lightModeIcon : darkModeIcon;
+}
+if( darkMode ) setDarkMode(darkMode);
 
